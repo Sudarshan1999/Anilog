f821aa34521de0ccd5e95bbd7e98183ae0787ccd
Alex Makoviecki <b2alexpilot@gmail.com>
2018-11-10 18:52:48 -0700

    Bootstrap navbar

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,8 +1,23 @@
 <template>
-    <div>
-        <h3>Upload Image</h3>
-        <input type="text" v-model="url" placeholder="Image URL"><br>
-        <button v-on:click="UploadImage">submit</button>
+    <div class = "site-wrapper">
+        <nav class = "navbar navbar-expand-lg">
+        <div class = "container-fluid">
+            <div class = "navbar-header">
+                <a class = "navbar-brand" href="#">Anilog</a>
+            </div>
+            <ul class = "navbar-nav mr-auto">
+                <li class = "nav-item"><a href="#">Page</a></li>
+                <li class = "nav-item"><a href="#">Page</a></li>
+                <li class = "nav-item"><a href="#">Page</a></li>
+                <li class = "nav-item"><a href="#">Page</a></li>
+            </ul>
+        </div>
+        </nav>
+        <div class = "container">
+            <h3>Upload Image</h3>
+            <input type="text" v-model="url" placeholder="Image URL"><br>
+            <button v-on:click="UploadImage">submit</button>
+        </div>
     </div>
 </template>
 
@@ -22,6 +37,7 @@ export default {
             apiKey: '806a013982f0402eb76adaa64b7a9acb'
             });
 
+      s
             clarifaiObject.models.predict(Clarifai.GENERAL_MODEL, this.url)
             .then(response=>{
                 var concepts = response['outputs'][0]['data']['concepts']
@@ -34,4 +50,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    @import '../assets/style/uploadImage.css';
 </style>
