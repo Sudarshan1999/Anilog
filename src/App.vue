76c662a0eddaaa2924e64b0d11b606bdb1091746
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2018-11-14 17:22:56 -0700

    Moved navbar to own component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,7 @@
 <template>
 	<div id="app">
 
-		<!-- Navigation -->
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-			<div class="container">
-				<span class="navbar-brand">Anilog</span>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarResponsive">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="#/">Home
-								<span class="sr-only">(current)</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#/searchUrl">Search URL</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#/uploadImage">Upload Image</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<Navbar />
 
 		<router-view />
 	</div>
@@ -32,8 +9,12 @@
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
 	name: 'App',
+	components: {
+		Navbar,
+	},
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="app">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div class="container">
+				<a href="#/">
+					<span class="navbar-brand">Anilog</span>
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="#/searchUrl">Search URL</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#/uploadImage">Upload Image</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: 'Navbar',
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+</style>
